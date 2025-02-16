42b36ebc941af15670d3d06d761d2598980d3951
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-16 20:48:39 +0100

    add rest api script to call job info

---
+++ rest_api_call_job.sh
@@ -0,0 +1,4 @@
+curl -L \
+-H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+https://api.github.com/repos/Kescefko/github-workflow-debugging-demo/actions/jobs/37303778000
