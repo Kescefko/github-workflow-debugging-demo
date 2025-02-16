8bbee5579ba23f29e423cb26a479edc895fa156b
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-16 20:51:57 +0100

    modify rest api endpoint

--- rest_api_call_job.sh
+++ rest_api_call_job.sh
@@ -1,4 +1,4 @@
 curl -L \
 -H "Accept: application/vnd.github+json" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/Kescefko/github-workflow-debugging-demo/actions/jobs/37303778000
+https://api.github.com/repos/Kescefko/github-workflow-debugging-demo/actions/jobs/37303778000/logs
